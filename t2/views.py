64943f82273d2f05c9c5610d5654725c0f8ef653
christttto <christttto@berkeley.edu>
2018-07-01 02:50:25 -0400

    t2

--- t2/views.py
+++ t2/views.py
@@ -3,3 +3,13 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 't2/index.html')
+def one(request):
+    return render(request, 't2/1.html')
+def two(request):
+    return render(request, 't2/2.html')
+def three(request):
+    return render(request, 't2/3.html')
+def four(request):
+    return render(request, 't2/4.html')
+def five(request):
+    return render(request, 't2/5.html')
